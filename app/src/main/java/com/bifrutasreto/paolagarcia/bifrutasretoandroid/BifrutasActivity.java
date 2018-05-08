80aa22619401bd6a0b972077e3f7b69d0bed6b4f
PAOLA GARCIA GARCIA <paolagarciagarcia@PAOLAs-MBP.home>
2018-05-08 15:50:24 +0200

    Taller for kids Mi primera  app android based on RetoBufrutas project by Paola Garcia

---
+++ app/src/main/java/com/bifrutasreto/paolagarcia/bifrutasretoandroid/BifrutasActivity.java
@@ -0,0 +1,14 @@
+package com.bifrutasreto.paolagarcia.bifrutasretoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BifrutasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bifrutas);
+
+    }
+}
